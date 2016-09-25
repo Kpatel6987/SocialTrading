f4b220f29f93ce937754c5fbffde9158fe042a44
Kavish Patel <kpatel6987@gmail.com>
2016-09-25 08:01:51 -0400

    Final Commit?

--- js/chart.js
+++ js/chart.js
@@ -28,3 +28,34 @@ $(function() {
     });
   });
 });
+$(document).ready(function() {
+    $("#clicker").click(function() {
+    var ticker = $("#acc").val();
+  var Aladdin = new blk.API();
+  Aladdin.performanceData({
+    identifiers: ticker
+  }, function(data) {
+    $('#container').highcharts('StockChart', {
+      rangeSelector: {
+        selected: 5
+      },
+      title: {
+        text: data.resultMap.RETURNS.map(function(returns) {
+          return returns.ticker
+        }).join('/') + ' Stock Return ($100 Investment)'
+      },
+      series: data.resultMap.RETURNS.map(function(returns) {
+        return {
+          name: returns.ticker,
+          data: returns.performanceChart.map(function(point) {
+            return [point[0], point[1] * 100]
+          }),
+          tooltip: {
+            valueDecimals: 2
+          }
+        }
+      })
+    });
+  });
+});
+});
